cadd3d4d0afb1a1f0057260a8247e01a1dea404a
Ben <540020707@qq.com>
2021-07-19 22:07:00 +0800

    modify request

--- vuetest/src/components/Login.vue
+++ vuetest/src/components/Login.vue
@@ -1,45 +1,49 @@
 <template>
   <div class="Login_class" style="margin-top: 50px">
-    <label>请输入账号：</label>
-    <el-input class="input_account" style="width: 200px;" v-model="requestData.account"></el-input>
+    <label style="margin-left: 50px;">请输入账号：</label>
+    <el-input style="width: 200px;" v-model="requestData.account"></el-input>
     <label style="margin-left: 200px;">请输入密码：</label>
-    <el-input class="input_account" style="width: 200px;" v-model="requestData.password"></el-input>
+    <el-input style="width: 200px;" v-model="requestData.password"></el-input>
     <br>
-    <el-button style="margin-left: 240px; margin-top: 50px;" v-on:click="submit()">提交</el-button>
+    <el-button style="margin-left: 290px; margin-top: 50px;" v-on:click="submit()" :loading="requestData.buttonLoading">
+      提交
+    </el-button>
   </div>
 </template>
 
 <script lang="js">
 import {defineComponent, reactive} from "vue";
-import request from "request";
-
 
 export default defineComponent({
   name: "Login",
   setup() {
     const requestData = reactive(
-          {
-            account: "",
-            password: "",
-          }
-      );
+        {
+          account: "",
+          password: "",
+          buttonLoading: false,
+        }
+    );
 
-    const submit = ()=> {
-      request({
+    const submit = () => {
+      requestData.buttonLoading = true;
+      this.request({
             url: "http://www.mockbin.com/hars",
             method: "POST",
             json: true,
             headers: {
-                "content-type": "application/json",
+              "content-type": "application/json",
             },
             body: JSON.stringify(requestData)
-        },
-        function (error, response) {
+          },
+          function (error, response) {
             if (!error && response.statusCode == 200) {
-                console.log("response.statusCode: ", response.statusCode); // 请求成功的处理逻辑
-                requestData.account = "graesegesgr";
+              console.log("response.statusCode: ", response.statusCode); // 请求成功的处理逻辑
             }
-        });
+            setTimeout(function () {
+              requestData.buttonLoading = false;
+            }, 6000)
+          });
     };
 
     return {
@@ -47,9 +51,7 @@ export default defineComponent({
       submit,
     };
   },
-  methods: {
-
-  }
+  methods: {}
 })
 </script>
 
